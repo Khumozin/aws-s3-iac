dc3683668bb865bd5e60e53e792b056c7ca4f690
Khumo Mogorosi <khumomogorosi@gmail.com>
2024-12-14 15:28:59 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+# tfsec:ignore:aws-s3-enable-bucket-logging
+resource "aws_s3_bucket" "bucket" {
+  bucket = "ultra-container"
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucket_ownership" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  depends_on = [ aws_s3_bucket_ownership_controls.bucket_ownership ]
+
+  bucket = aws_s3_bucket.bucket.id
+  acl = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket_public_access" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "bucket_versioning" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_kms_key" "kms_key" {
+  description = "This key is used to encrypt bucket objects"
+  enable_key_rotation = true
+  deletion_window_in_days = 7
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_server_encryption_config" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.kms_key.arn
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
